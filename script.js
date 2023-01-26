7fc88917201cf112f5bfd2a0a650eb858258e11a
Reidner Willians Pacheco Peres <pacheco195920@gmail.com>
2023-01-25 21:29:58 -0300

    update script

---
+++ script.js
@@ -0,0 +1,21 @@
+const dummyTransactions = [
+    {id: 1, name: 'Bolo de Brigadeiro', amount: -20},
+    {id: 2, name: 'Salário', amount: 300},
+    {id: 3, name: 'Torta de frango', amount: -10},
+    {id: 4, name: 'Violão', amount: 150}
+]
+
+const addTransactionIntoDOM = transaction => {
+    const operator = transaction.amount < 0 ? '-' : '+'
+    const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
+    const amountWithoutoperator = Math.abs(transaction.amount)
+    const li = document.createElement('li')
+
+    li.classList.add(CSSClass)
+    li.innerHTML = ` 
+        ${transaction.name} <span>${operator} R$ ${Math.abs(transaction.amount)} </span><button Class="delete.btn">x</button>
+    `
+    console.log(li)            
+}
+
+addTransactionIntoDOM(dummyTransactions[0])
