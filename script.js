f9b3da391f90a4e3b59869abc617b49af28f4cb4
Reidner Willians Pacheco Peres <pacheco195920@gmail.com>
2023-01-27 21:05:42 -0300

    Update Script

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const transactionsUl = document.querySelector('#transactions')
+const incomeDisplay = document.querySelector('#money-plus')
+const expenseDisplay = document.querySelector('#money-minus')
+const balanceDisplay = document.querySelector('#balance')
 
 const dummyTransactions = [
     {id: 1, name: 'Bolo de Brigadeiro', amount: -20},
@@ -21,8 +24,24 @@ const addTransactionIntoDOM = transaction => {
 }
 
 const updateBalanceValues = () => {
-    const transactionsAmounts = dummyTransactions.map(transaction => transaction.amount)
-    console.log(transactionsAmounts)
+    const transactionsAmounts = dummyTransactions
+        .map(transaction => transaction.amount)
+    const total = transactionsAmounts
+        .reduce((accumulator, transaction) => accumulator + transaction, 0)
+        .toFixed(2)
+    const income = transactionsAmounts
+        .filter(value => value > 0)
+        .reduce((accumulator, value) => accumulator + value, 0)
+        .toFixed(2)
+    const expense = transactionsAmounts
+        .filter(value => value < 0)
+        .reduce((accumulator, value) => accumulator + value, 0)
+        .toFixed(2)
+    balanceDisplay.textContent = `R$ ${total}`
+    incomeDisplay.textContent = `R$ ${income}`
+    expenseDisplay.textContent = `R$ ${expense}`
+
+    console.log(expense)
 }
 
 const init = () => {
